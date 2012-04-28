031019a8cdecbe574539be9a6213d9c4586e3f04
Norbert Crombach <norbert.crombach@primetheory.org>
2012-04-28 15:59:37 +0200

    Register callback when including model helper

--- lib/stale/model.rb
+++ lib/stale/model.rb
@@ -2,6 +2,12 @@ module Stale
   module Model
     IDENTIFIER = :stale_key
 
+    def self.included(base)
+      base.class_eval do
+        after_commit :expire_stale_dependencies if respond_to?(:after_commit)
+      end
+    end
+
     def stale_id
       to_param
     end
